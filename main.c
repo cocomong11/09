5ba1d2b1353e0f3f96f8ffaec0fc7a7fef54fd37
cocomong11 <carpediem1011@naver.com>
2025-10-30 15:58:14 +0900

    Update main.c

--- main.c
+++ main.c
@@ -3,18 +3,20 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 # define SIZE 5
-int main(int argc, char *argv[]) {
-	int i,average;
-	int sum=0;
+int main(void){
+	int i;
 	int grade[SIZE];
-
-	for(i=0;i<SIZE;i++){
+	int score[SIZE];
 	
-	printf("학생성적을 입력하세요:");
-	scanf("%d",&grade[i]);
-	sum+=grade[i];
-}
-	average=sum/SIZE;
-	printf("성적 평균:%d\n",average);
-	return 0;
+	for(i=0;i<SIZE;i++){
+	grade[i]=rand()%100;
+		}
+	for (i=0;i<SIZE;i++){
+	score[i]=grade[i];
+	}
+	
+	for (i=0;i<SIZE;i++)
+	printf("grade[%i]=%i,score[%i]=%i\n",i,grade[i],i,score[i]);
+	
+	return 0; 
 }
